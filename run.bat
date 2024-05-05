ba254d1fa0fc8953cb5d406d1a9917f765ebc41e
rubatotree <zhuyutian@mail.ustc.edu.cn>
2024-05-06 00:23:57 +0800

    Windows Support

--- run.bat
+++ run.bat
@@ -1,13 +1,22 @@
 @echo off
 rmdir /s /q output
-mkdir .\output\temp
+mkdir .\output\image
+mkdir .\output\image\temp
 
-python3 ./src/main.py ./output/image.ppm || goto failed
+pypy3 ./src/main.py image || goto failed
 
 :succeed
 echo "===RUN SUCCESS==="
-ffmpeg -loglevel quiet -y -i ./output/image.ppm ./output/image.bmp
-ffmpeg -f image2 -r 20 -i "./output/temp/%%01d.jpg" ./output/image.gif
+if exist ".\output\image\image.ppm" (
+    ffmpeg -loglevel quiet -y -i ./output/image/image.ppm ./output/image/image.bmp
+    if exist ".\output\image\temp\0.ppm" (
+        ffmpeg -f image2 -r 20 -i "./output/image/temp/%%01d.ppm" ./output/image/image.gif
+    )
+) else (
+    if exist ".\output\image\temp\0.jpg" (
+        ffmpeg -f image2 -r 20 -i "./output/image/temp/%%01d.jpg" ./output/image/image.gif
+    )
+)
 exit 0
 
 :failed
